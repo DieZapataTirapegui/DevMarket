547026b4cb884eba3eaff51e10e7742acef9b9c8
dzapatti <dzapatti@emeal.nttdata.com>
2026-05-29 18:41:53 -0400

    Cambios Correccion

--- devmarket-api/src/main.ts
+++ devmarket-api/src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://172.20.114.180:5173',  // ← agregar esto
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
